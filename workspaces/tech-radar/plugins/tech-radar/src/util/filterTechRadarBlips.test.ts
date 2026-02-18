902035b85ed9d27099db2084a2438cb668805418
Emiel van Lankveld <evanlankveld@bol.com>
2026-02-18 16:23:47 +0100

    'Fix' test.

--- workspaces/tech-radar/plugins/tech-radar/src/util/filterTechRadarBlips.test.ts
+++ workspaces/tech-radar/plugins/tech-radar/src/util/filterTechRadarBlips.test.ts
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import type { Blip } from './types';
+import type { Blip } from '../types';
 
 import { filterBlips } from './filterTechRadarBlips';
 
